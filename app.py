fa37674788dbfe6f830dc3c0bc4b40973696fc4a
pjenkins7 <88481007+pjenkins7@users.noreply.github.com>
2025-05-09 15:40:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ if existing_cols:
         'Completed Days': df['Completed Days'].sum(),
         'Completed Books': df['Completed Books'].sum(),
         'Book Pages': df['Book Pages'].sum(),
-        'Status': 
+        'Status': '✅ Completed'
     }
     df_totals = pd.concat([df_totals, pd.DataFrame([totals_row])], ignore_index=True)
     st.dataframe(df_totals)
